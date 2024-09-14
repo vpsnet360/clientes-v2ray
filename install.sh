a3a7adcb9d2b71a513041094183161e2de267b54
net_vps <101622761+vpsnet360@users.noreply.github.com>
2024-09-14 12:10:54 -0300

    Add files via upload

--- install.sh
+++ install.sh
@@ -98,7 +98,7 @@ if grep -q "alias v2=" ~/.bashrc; then
     sed -i '/alias v2=/d' ~/.bashrc
 fi
 
-wget --no-cache -O /usr/bin/v2.sh https://raw.githubusercontent.com/joaquin1444/clientes-v2ray/main/v2.sh > /dev/null 2>&1
+wget --no-cache -O /usr/bin/v2.sh https://raw.githubusercontent.com/vpsnet360/clientes-v2ray/main/v2.sh > /dev/null 2>&1
 if [ $? -eq 0 ]; then
     sudo chmod +x /usr/bin/v2.sh
     echo "alias v2='/usr/bin/v2.sh'" | sudo tee -a ~/.bashrc > /dev/null
